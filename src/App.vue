e440f01b7042b1d943918a24a4c3265fabf64a10
TravkinEvgeny <asassinev@yandex.ru>
2021-01-30 14:27:16 +0300

    created navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,103 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <router-link class="navbar-brand" to="/">Tech</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav row w-100">
+            <li class="navbar__item dropdown col">
+              <p class="navbar__link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Каталог
+              </p>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <router-link v-for="(link, id) in catalog" :key="id" class="dropdown-item" :to="link.path">
+                  {{ link.title }}
+                </router-link>
+              </div>
+            </li>
+            <li class="navbar__item col" v-for="(link, id) in navbar" :key="id">
+              <router-link class="navbar__link" :to="link.path">
+                <i :class="link.iconClass"></i>
+                {{ link.title }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- <div class="navbar">
+      <div class="container">
+        <router-link class="navbar__title" to="/">Tech</router-link>
+        <div class="row text-center navbar-width">
+          <div class="navbar__item col-3">
+            <div class="dropdown">
+              <p class="dropdown-toggle navbar__link" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Каталог
+              </p>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <router-link v-for="(link, id) in catalog" :key="id" class="dropdown-item" :to="link.path">{{ link.title }}</router-link>
+              </div>
+            </div>
+          </div>
+          <div class="navbar__item col-3" v-for="(link, id) in navbar" :key="id">
+            <router-link class="navbar__link" :to="link.path">
+              <i :class="link.iconClass"></i>
+              {{ link.title }}
+              </router-link>
+          </div>
+        </div>
+      </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      navbar: [
+        {
+          iconClass: 'fas fa-phone-square-alt',
+          path: '/contacts',
+          title: ' Контакты'
+        },
+        {
+          iconClass: 'fas fa-shopping-cart',
+          path: '/shopping-cart',
+          title: ' Корзина'
+        },
+        {
+          iconClass: 'fas fa-user-circle',
+          path: '/profile',
+          title: ' Профиль'
+        }
+      ],
+      catalog: [
+        {
+          path: '/desktop',
+          title: 'Компьютеры'
+        },
+        {
+          path: '/notebook',
+          title: 'Ноутбуки'
+        },
+        {
+          path: '/computer-components',
+          title: 'Комплектующие'
+        },
+        {
+          path: '/smartphone',
+          title: 'Смартфоны'
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -17,16 +108,55 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.navbar {
+  // width: 100%;
+  // height: 60px;
+  box-shadow: 0 0 10px rgba(0,0,0,0.5);
 
-  a {
-    font-weight: bold;
+  &-width {
+    width: 95%;
+  }
+
+  &__title {
+    font-size: 28px;
+    margin: 0;
     color: #2c3e50;
+    text-decoration: none;
+    &:hover {
+      color: #6092c4;
+      text-decoration: none;
+    }
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  &__item {
+    padding: 6px 0;
+    font-size: 16px;
+    @media (min-width: 992px) {
+      padding: 0;
+      font-size: 18px;
+    }
+  }
+
+  &__link {
+    width: fit-content;
+    margin: 0 auto;
+    color: #2c3e50;
+    text-decoration: none;
+
+    &:hover {
+      cursor: pointer;
+      color: #6092c4;
+      text-decoration: none;
     }
   }
 }
+
+.dropdown {
+  color: #2c3e50;
+  &-menu {
+    top: 168%;
+    left: 30%;
+  }
+}
+
 </style>
