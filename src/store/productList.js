94c82f478f02c4a7e7ed37b403c11104894a8cbc
TravkinEvgeny <asassinev@yandex.ru>
2021-03-16 17:17:31 +0300

    clear productList store

--- src/store/productList.js
+++ src/store/productList.js
@@ -2,31 +2,15 @@ import axios from 'axios'
 
 export default {
   state: {
-    list: null,
-    newList: null,
-    filters: null,
-    maxPrice: 0,
-    sortParams: ''
+    list: null
   },
   mutations: {
     setList (state, payload) {
       state.list = payload
-    },
-    setNewList (state, payload) {
-      state.newList = payload
-    },
-    setFilters (state, payload) {
-      state.filters = payload
-    },
-    setMaxPrice (state, payload) {
-      state.maxPrice = payload
-    },
-    setSortParams (state, payload) {
-      state.sortParams = payload
     }
   },
   actions: {
-    async fetchList ({ commit, getters }, payload) {
+    async fetchList ({ commit }, payload) {
       commit('setLoading', true)
       await axios({
         method: 'get',
@@ -38,38 +22,16 @@ export default {
       })
         .then(response => {
           commit('setList', response.data)
-          commit('setMaxPrice', getters.getMaxPrice)
         })
         .catch(error => {
           console.log(error)
         })
       commit('setLoading', false)
-    },
-    setSortParams ({ commit }, payload) {
-      commit('setSortParams', payload)
     }
   },
   getters: {
     getList (state) {
       return state.list
-    },
-    getFilters (state) {
-      return state.filters
-    },
-    getMaxPrice (state) {
-      var max = 0
-      state.list.forEach(p => {
-        if (parseInt(p.price.split(' ').join('')) > max) {
-          max = parseInt(p.price.split(' ').join(''))
-        }
-      })
-      return max
-    },
-    maxPrice (state) {
-      return state.maxPrice
-    },
-    getSortParams (state) {
-      return state.sortParams
     }
   }
 }
