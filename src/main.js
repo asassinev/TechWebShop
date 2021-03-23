92db4fd68c1f150c85bc2896ea764b1c67b51b58
TravkinEvgeny <asassinev@yandex.ru>
2021-03-23 13:53:33 +0300

    added yandex map plugin

--- src/main.js
+++ src/main.js
@@ -7,9 +7,10 @@ import App from './App.vue'
 import Loader from './components/shared/Loader'
 import StarRating from './components/shared/starRating'
 
-import VueEllipseProgress from 'vue-ellipse-progress'
 import * as mdb from 'mdb-ui-kit'
 import '../node_modules/mdb-ui-kit/css/mdb.min.css'
+import VueEllipseProgress from 'vue-ellipse-progress'
+import YmapPlugin from 'vue-yandex-maps'
 
 Vue.component('Loader', Loader)
 Vue.component('v-rating', StarRating)
@@ -17,6 +18,7 @@ Vue.component('v-rating', StarRating)
 Vue.use(Vuelidate)
 Vue.use(mdb)
 Vue.use(VueEllipseProgress)
+Vue.use(YmapPlugin)
 
 Vue.config.productionTip = false
 
