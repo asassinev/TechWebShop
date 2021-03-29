f462e6ad7c1fa1b8a997846972a9a08613449e0f
TravkinEvgeny <asassinev@yandex.ru>
2021-03-29 23:56:57 +0300

    moved bind for phone

--- src/main.js
+++ src/main.js
@@ -19,6 +19,29 @@ Vue.use(YmapPlugin)
 
 Vue.config.productionTip = false
 
+Vue.directive('phone', {
+  bind (el) {
+    el.oninput = function (e) {
+      if (!e.isTrusted) {
+        return
+      }
+      const x = this.value
+        .replace(/\D/g, '')
+        .match(/(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/)
+      x[1] = '+7'
+      this.value = !x[3]
+        ? x[1] + x[2]
+        : x[1] +
+          ' (' +
+          x[2] +
+          ') ' +
+          x[3] +
+          (x[4] ? '-' + x[4] : '') +
+          (x[5] ? '-' + x[5] : '')
+    }
+  }
+})
+
 new Vue({
   router,
   store,
--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -381,7 +381,6 @@
 </template>
 
 <script>
-import Vue from 'vue'
 import calendar from '../components/checkout/calendar.vue'
 
 export default {
@@ -402,26 +401,4 @@ export default {
     }
   }
 }
-Vue.directive('phone', {
-  bind (el) {
-    el.oninput = function (e) {
-      if (!e.isTrusted) {
-        return
-      }
-      const x = this.value
-        .replace(/\D/g, '')
-        .match(/(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/)
-      x[1] = '+7'
-      this.value = !x[3]
-        ? x[1] + x[2]
-        : x[1] +
-          ' (' +
-          x[2] +
-          ') ' +
-          x[3] +
-          (x[4] ? '-' + x[4] : '') +
-          (x[5] ? '-' + x[5] : '')
-    }
-  }
-})
 </script>
