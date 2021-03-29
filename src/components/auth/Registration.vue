a47b58ab67068a4fb7bf2cbbc48fde3680aeb193
TravkinEvgeny <asassinev@yandex.ru>
2021-03-30 00:03:37 +0300

    updated registration page

--- src/components/auth/Registration.vue
+++ src/components/auth/Registration.vue
@@ -1,84 +1,81 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <form
-        class="col-sm-12 col-lg-6 form shadow p-3 bg-white rounded"
-        @submit.prevent="submit"
-        novalidate
-      >
-        <h1 class="text-center fs-2 text-primary m-0">Регистрация</h1>
-        <hr />
-        <div class="form-floating mb-3" :class="{ errors: emailErrors[0] }">
-          <input
-            @input="$v.email.$touch()"
-            @blur="$v.email.$touch()"
-            class="form-control form-control-md"
-            type="email"
-            v-model="email"
-            placeholder="Email address"
-            id="email"
-            required
-          />
-          <label for="floatingInput">Email address</label>
-          <div
-            v-if="emailErrors[0]"
-            :class="{ 'invalid-feedback': emailErrors[0] }"
-          >
-            {{ emailErrors[0] }}
-          </div>
-        </div>
-        <div class="form-floating mb-3" :class="{ errors: passwordErrors[0] }">
-          <input
-            @input="$v.password.$touch()"
-            @blur="$v.password.$touch()"
-            class="form-control form-control-md"
-            id="password"
-            v-model="password"
-            placeholder="Password"
-            type="password"
-            required
-          />
-          <label class="form-label" for="floatingInput">Password</label>
-          <div
-            v-if="passwordErrors[0]"
-            :class="{ 'invalid-feedback': passwordErrors[0] }"
-          >
-            {{ passwordErrors[0] }}
-          </div>
-        </div>
+  <div class="d-flex justify-content-center">
+    <form
+      class="form shadow p-3 bg-white rounded"
+      @submit.prevent="submit"
+      novalidate
+    >
+      <h1 class="text-center fs-2 text-primary m-0">Регистрация</h1>
+      <hr />
+      <div class="form-floating mb-3" :class="{ errors: emailErrors[0] }">
+        <input
+          @input="$v.email.$touch()"
+          @blur="$v.email.$touch()"
+          class="form-control form-control-sm"
+          type="email"
+          v-model="email"
+          placeholder="Email address"
+          id="email"
+          required
+        />
+        <label for="floatingInput">Email address</label>
         <div
-          class="form-floating mb-3"
-          :class="{ errors: repeatPasswordErrors[0] }"
+          v-if="emailErrors[0]"
+          :class="{ 'invalid-feedback': emailErrors[0] }"
         >
-          <input
-            @input="$v.repeatPassword.$touch()"
-            @blur="$v.repeatPassword.$touch()"
-            class="form-control form-control-md"
-            id="repeatPassword"
-            v-model="repeatPassword"
-            placeholder="Repeat password"
-            type="password"
-            required
-          />
-          <label class="form-label" for="floatingInput">Repeat password</label>
-          <div
-            v-if="repeatPasswordErrors[0]"
-            :class="{ 'invalid-feedback': repeatPasswordErrors[0] }"
-          >
-            {{ repeatPasswordErrors[0] }}
-          </div>
+          {{ emailErrors[0] }}
         </div>
-        <div class="text-right">
-          <button
-            type="button"
-            class="mr-2 btn btn-primary btn-lg w-100"
-            @click="submit"
-          >
-            Создать аккаунт
-          </button>
+      </div>
+      <div class="form-floating mb-3" :class="{ errors: passwordErrors[0] }">
+        <input
+          @input="$v.password.$touch()"
+          @blur="$v.password.$touch()"
+          class="form-control form-control-sm"
+          id="password"
+          v-model="password"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <label class="form-label" for="floatingInput">Password</label>
+        <div
+          v-if="passwordErrors[0]"
+          :class="{ 'invalid-feedback': passwordErrors[0] }"
+        >
+          {{ passwordErrors[0] }}
         </div>
-      </form>
-    </div>
+      </div>
+      <div
+        class="form-floating mb-3"
+        :class="{ errors: repeatPasswordErrors[0] }"
+      >
+        <input
+          @input="$v.repeatPassword.$touch()"
+          @blur="$v.repeatPassword.$touch()"
+          class="form-control form-control-sm"
+          id="repeatPassword"
+          v-model="repeatPassword"
+          placeholder="Repeat password"
+          type="password"
+          required
+        />
+        <label class="form-label" for="floatingInput">Repeat password</label>
+        <div
+          v-if="repeatPasswordErrors[0]"
+          :class="{ 'invalid-feedback': repeatPasswordErrors[0] }"
+        >
+          {{ repeatPasswordErrors[0] }}
+        </div>
+      </div>
+      <div class="d-flex justify-content-between align-items-end flex-wrap">
+        <router-link to="/login" class="pb-2 fs-7" @click="submit">
+          Использовать уже существущий аккаунт
+        </router-link>
+        <button type="button" class="btn btn-primary" @click="submit">
+          Создать аккаунт
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -109,7 +106,16 @@ export default {
       sameAsPassword: sameAs('password')
     }
   },
+  watch: {
+    User () {
+      console.log(this.User)
+      this.$router.push('/profile')
+    }
+  },
   computed: {
+    User () {
+      return this.$store.getters.getUs
+    },
     emailErrors () {
       const errors = []
       if (!this.$v.email.$dirty) return errors
@@ -141,23 +147,9 @@ export default {
           email: this.email,
           password: this.password
         }
-        this.$store.dispatch('createUser', user).finally(() => {
-          this.$store.dispatch('setNotification', {
-            title: 'success',
-            text: 'Аккаунт успешно создан'
-          })
-          this.$router.push('/login')
-        })
+        this.$store.dispatch('createUser', user)
       }
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.invalid-feedback {
-  position: relative;
-  display: block;
-  margin: 4px 0 0 8px;
-}
-</style>
