3ebbb9ee026a4e74ef10944b7389e2cbcd290ac9
evgeny travkin <asassinev@yandex.ru>
2021-04-06 21:22:04 +0300

    updated API for catalog store

--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -1,24 +1,50 @@
 <template>
   <div>
-    <h1 class="text-left">Каталог</h1>
-    <Categories :catalog="catalog" />
+    <loader class="mt-4" v-if="loading" />
+    <div v-else>
+      <h1 class="text-left">Каталог</h1>
+      <div class="row">
+        <div
+          class="col-sm-6 col-xs-12 col-lg-3"
+          v-for="item in catalog.items"
+          :key="item.id"
+        >
+          <CategoryCard :item="item"></CategoryCard>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Categories from '../components/catalog/Categories'
+import CategoryCard from '../components/catalog/CategoryCard'
+import Loader from '../components/shared/Loader.vue'
 
 export default {
   computed: {
     catalog () {
-      return this.$store.getters.catalog
+      return this.$store.getters.getCatalog
+    },
+    loading () {
+      return this.$store.getters.getLoading
+    },
+    paramsCatalog () {
+      return this.$route.params.name
+    }
+  },
+  watch: {
+    paramsCatalog () {
+      this.$store.dispatch('fetchCatalog', this.paramsCatalog)
     }
   },
   created () {
-    this.$store.dispatch('getCatalog', this.$route.params.name)
+    if (this.catalog.length === 0) {
+      this.$store.dispatch('fetchCatalog', this.paramsCatalog)
+    }
   },
   components: {
-    Categories
+    CategoryCard,
+    Loader
   }
 }
 </script>
